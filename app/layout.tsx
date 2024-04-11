c75feb061da6ad4ac0c52228c39bc53735711ffc
Matheiu Perdido <matheiuamiel@gmail.com>
2024-04-12 05:26:40 +0800

    Add login page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { MantineProvider } from "@mantine/core";
+import '@mantine/core/styles.css';
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <MantineProvider>{children}</MantineProvider>
+      </body>
     </html>
   );
 }
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,12 +3,15 @@ import { headers } from "next/headers";
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 import { SubmitButton } from "./submit-button";
+import { Box, Button, Container, Flex, Text, TextInput } from "@mantine/core";
+import { Auth } from "@supabase/auth-ui-react";
 
 export default function Login({
   searchParams,
 }: {
   searchParams: { message: string };
 }) {
+  const supabase = createClient();
   const signIn = async (formData: FormData) => {
     "use server";
 
@@ -51,69 +54,75 @@ export default function Login({
     return redirect("/login?message=Check email to continue sign in process");
   };
 
+  const loginWithGoogle = async () => {
+    "use server";
+    const supabase = createClient();
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: "http://localhost:3000/protected",
+      },
+    });
+  };
+
   return (
-    <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
+    <Container className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
       <Link
         href="/"
         className="absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="mr-2 h-4 w-4 transition-transform group-hover:-translate-x-1"
-        >
-          <polyline points="15 18 9 12 15 6" />
-        </svg>{" "}
         Back
       </Link>
 
-      <form className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
-        <label className="text-md" htmlFor="email">
-          Email
-        </label>
-        <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
-          name="email"
-          placeholder="you@example.com"
-          required
-        />
-        <label className="text-md" htmlFor="password">
-          Password
-        </label>
-        <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
-          type="password"
-          name="password"
-          placeholder="••••••••"
-          required
-        />
-        <SubmitButton
-          formAction={signIn}
-          className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2"
-          pendingText="Signing In..."
-        >
-          Sign In
-        </SubmitButton>
-        <SubmitButton
-          formAction={signUp}
-          className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2"
-          pendingText="Signing Up..."
-        >
-          Sign Up
-        </SubmitButton>
-        {searchParams?.message && (
-          <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
-            {searchParams.message}
-          </p>
-        )}
+      <form className="animate-in flex-1 flex flex-col w-full justify-center gap-5 text-foreground">
+        <Flex direction="column" gap={4}>
+          <Box>
+            <TextInput
+              withAsterisk
+              label="Email"
+              name="email"
+              type="email"
+              placeholder="your@email.com"
+              required
+            />
+          </Box>
+          <Box>
+            <TextInput
+              withAsterisk
+              label="Password"
+              name="password"
+              type="password"
+              placeholder="••••••••"
+              required
+            />
+          </Box>
+          <Box pt={5}>
+            <SubmitButton
+              formAction={signIn}
+              className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2"
+              pendingText="Signing In..."
+            >
+              Sign In
+            </SubmitButton>
+            <SubmitButton
+              formAction={signUp}
+              className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2"
+              pendingText="Signing Up..."
+            >
+              Sign Up
+            </SubmitButton>
+          </Box>
+          <Text>Or</Text>
+          <Box>
+            <Button onClick={loginWithGoogle}>Login with google</Button>
+          </Box>
+          {searchParams?.message && (
+            <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
+              {searchParams.message}
+            </p>
+          )}
+        </Flex>
       </form>
-    </div>
+    </Container>
   );
 }
